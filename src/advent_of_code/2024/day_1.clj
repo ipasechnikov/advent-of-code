fdd6b7cdd36e57668ec4e0ac5a3905f7c3b7027b
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2025-01-18 02:30:24 +0900

    Solve 2024-12-01

---
+++ src/advent_of_code/2024/day_1.clj
@@ -0,0 +1,43 @@
+(ns advent-of-code.2024.day-1
+  (:require
+   [clojure.string :as str]))
+
+(defn parse-input-file
+  [input-file-path]
+  (let [input-rows (map #(str/split % #"\s+")
+                        (str/split-lines (slurp input-file-path)))
+        input-lst1 (map #(Integer/parseInt (first %)) input-rows)
+        input-lst2 (map #(Integer/parseInt (second %)) input-rows)]
+    [input-lst1 input-lst2]))
+
+(defn part-1
+  [input-file-path]
+  (let [[input-lst1 input-lst2] (parse-input-file input-file-path)
+        sorted-input-lst1 (sort input-lst1)
+        sorted-input-lst2 (sort input-lst2)
+        zipped-input-lsts (map vector sorted-input-lst1 sorted-input-lst2)]
+    (reduce (fn [acc [x y]]
+              (+ acc (abs (- x y))))
+            0
+            zipped-input-lsts)))
+
+(defn part-2
+  [input-file-path]
+  (let [[input-lst1 input-lst2] (parse-input-file input-file-path)
+        input-lst2-freq (reduce (fn [acc v]
+                                  (update acc v (fnil inc 0)))
+                                {}
+                                input-lst2)]
+    (reduce (fn [acc v]
+              (let [v-freq (get input-lst2-freq v 0)]
+                (+ acc (* v v-freq))))
+            0
+            input-lst1)))
+
+(comment
+  (part-1 "input/2024/day-1.txt")
+  ;;=> 2113135
+
+  (part-2 "input/2024/day-1.txt")
+  ;;=> 19097157
+  :rcf)
